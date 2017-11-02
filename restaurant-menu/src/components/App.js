8e73a7cf33819460c2796ee0c6b5f5b77ce12be4
Man Hong Lee <manhong323@gmail.com>
2017-11-02 09:01:11 -0400

    Add local sample data Add to order button

--- restaurant-menu/src/components/App.js
+++ restaurant-menu/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 
 class App extends React.Component {
 
@@ -12,6 +14,8 @@ class App extends React.Component {
             order:{}
         }
         this.AddFish = this.AddFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+        this.AddOrder = this.AddOrder.bind(this);
     }
 
     AddFish(fish){
@@ -21,14 +25,37 @@ class App extends React.Component {
         this.setState({fishes: fishes});
     }
 
+    loadSamples(){
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
+
+    AddOrder(key){
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState(
+            {order}
+        );
+    }
+
     render(){
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header age="5000" yes={true} tagline="Fresh Seafood Market"/>
+                    <ul className="list">
+                        {Object
+                            .keys(this.state.fishes)
+                            .map(key => <Fish 
+                            key={key}
+                            index={key}
+                            details={this.state.fishes[key]}
+                            AddOrder={this.AddOrder}/>)}
+                    </ul>
                 </div>
                 <Order /> 
-                <Inventory AddFish={this.AddFish} />
+                <Inventory AddFish={this.AddFish} loadSamples={this.loadSamples}/>
             </div>
         )
     }
