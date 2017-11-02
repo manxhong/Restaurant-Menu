1494a01220f665a7555541fcb26a1a4a96acfab3
Man Hong Lee <manhong323@gmail.com>
2017-11-02 12:11:01 -0400

    Persisting State with Firebase

---
+++ restaurant-menu/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyB9_iO3FdGSMCWp0lVUgkH82Kde-schQLg",
+    authDomain: "restaurant-menu-8bde5.firebaseapp.com",
+    databaseURL: "https://restaurant-menu-8bde5.firebaseio.com",
+});
+
+export default base;
--- restaurant-menu/src/components/App.js
+++ restaurant-menu/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order'
 import Inventory from './Inventory'
 import sampleFishes from '../sample-fishes'
 import Fish from './Fish'
+import base from '../base'
 
 class App extends React.Component {
 
@@ -16,6 +17,18 @@ class App extends React.Component {
         this.AddFish = this.AddFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.AddOrder = this.AddOrder.bind(this);
+    }
+
+    componentWillMount(){
+        this.ref = base.syncState(`${this.props.params.storeId}/fishes`,
+        {
+            context: this,
+            state: 'fishes'
+    })
+    }
+
+    componentWillUnmount(){
+        base.removeBinding(this.ref); 
     }
 
     AddFish(fish){
